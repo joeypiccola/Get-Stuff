74648f29bd5284f6011d7fd2cb387d7449ec1da5
joeypiccola <joey.piccola@gmail.com>
2016-10-31 22:17:53 -0600

    initial commit

---
+++ Get-Stuff.Tests.ps1
@@ -0,0 +1,34 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+Import-Module "$here\$sut" -Force
+
+Describe "Get-Stuff" {
+    Context "Test input methods" {
+        it "Accepts pipeline input by property name" {
+            (New-Object -TypeName psobject -Property @{Entry = 2} | Get-Stuff).entry | Should be 2
+        }
+        it "Accept pipeline input by value" {
+            (3 | Get-Stuff).entry | Should be 3
+        }
+        it "Accept value my parameter" {
+            (Get-Stuff -entry 4).entry | should be 4
+        }
+    }
+    Context "Returns objects as desired"{
+        it "Output is pscustomobject" {
+            (Get-Stuff -entry 4).gettype().name | Should be 'pscustomobject'
+        }
+
+        it "Entry property returns int" {
+            (Get-Stuff -entry 5).entry.gettype().name | Should be ([int32]).name
+        }
+
+        it "String property returns string" {
+            (Get-Stuff -entry 6).mystring.gettype().name | Should be ([string]).name
+        }
+    
+        it "Weekday 1 returns Monday" {
+            (get-stuff -entry 1).dayofweek | Should be 'Monday'
+        }
+    }
+}
---
+++ Get-Stuff.ps1
@@ -0,0 +1,26 @@
+﻿function Get-Stuff
+{
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory,ValueFromPipeline,valuefrompipelinebypropertyname)]
+        [ValidateRange(1,6)] 
+        [int]$entry        
+    )
+    begin
+    {}
+    process
+    {
+        $output = [PSCustomObject]@{
+            Entry = $entry
+            DayOfWeek = $null
+            MyString = "My entry is $entry"
+        }
+
+        $output.dayofweek = [dayofweek]$entry
+        return $output
+    }
+    end
+    {}
+
+}
